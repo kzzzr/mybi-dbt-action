806422937deaf758f19408e6b7366e944749e055
Artemiy Kozyr <a.kozyr@wheely.com>
2021-02-25 02:32:26 +0300

    write commands, exit when any error occurs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 
+set -ex
+
 echo "dbt --version" && dbt --version
 echo "dbt debug" && dbt debug
 echo "dbt deps" && dbt deps
 echo "dbt seed" && dbt seed
 echo "dbt run" && dbt run
-echo "dbt test" && dbt test
+echo "dbt test" && dbt test`
